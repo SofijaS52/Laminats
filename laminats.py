bbfa8353c10ec541b1fc5192a8911b6d319c2260
SofijaS52 <sstarostina@edu.riga.lv>
2024-12-03 07:55:18 +0000

    ievade2

--- laminats.py
+++ laminats.py
@@ -29,5 +29,9 @@ def aprekinat_laminatu(plaksnes_garums, plaksnes_platums, istabas_garums, istaba
 
 # pārbaude
 print(aprekinat_laminatu(1, .5, 3, 2))
-istabas_garums = float(input("ievadīt"))
-istabas_platums = float(input("ievadīt"))
+istabas_garums = float(input("ievadīt:"))
+istabas_platums = float(input("ievadīt:"))
+plaksnes_garums = float(input("ievadīt:"))
+plaksnes_platums = float(input("ievadīt:"))
+pakas_cena = float(input("ievadīt:"))
+pakas_platiba = float(input("ievadīt:"))
